e417751cfacea6dc132e50c4909d55a34336ae02
Nguyen Duc Nhat <iamducnhat@gmail.com>
2025-09-26 21:58:26 +0700

    Update Vercel configuration and enhance Flask app with HTML rendering
    - Updated vercel.json to version 2, added builds and routes for API handling.
    - Modified api/index.py to serve an HTML template instead of plain text.
    - Added new index.html template for rendering a basic webpage.

--- api/index.py
+++ api/index.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(
+    __name__,
+    static_folder="../static",    # static folder relative to this file
+    template_folder="../templates"
+)
 
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    return render_template('index.html')
 
 @app.route('/about')
 def about():
